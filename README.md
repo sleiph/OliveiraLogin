e674dc08aaab78f7c743382f54e1c90a0512dbd7
Ricardo Gomes <sleiph@gmail.com>
2022-02-15 14:48:52 -0300

    o inicio de um sonho

---
+++ README.md
@@ -0,0 +1,16 @@
+# Oliveira Login
+
+Sistema de cadastro e login de usuários, desenvolvido pro programa Best Minds 2022.
+
+### Como usar
+```shell
+git clone https://github.com/sleiph/OliveiraLogin.git
+cd OliveiraLogin
+```
+```shell
+npm install
+npm start
+```
+
+### Tecnologias
+Node.js | [Express](https://www.npmjs.com/package/express) | [Bcrypt](https://www.npmjs.com/package/bcrypt) | [Body-parser](https://www.npmjs.com/package/body-parser)
---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const http = require('http');
+const bcrypt = require('bcrypt');
+const path = require("path");
+const bodyParser = require('body-parser');
+
+const users = require('./data').userDB;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
+
+app.post('/cadastro', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (!foundUser) {
+    
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
+    
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('Lista de Usuários:', users);
+    
+            res.send("<div align ='center'><h2>Cadastro efetuado.</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./cadastro.html'>Cadastrar outro usuário</a></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email já cadastrado.</h2></div><br><br><div align='center'><a href='./cadastro.html'>Cadastrar novamente.</a></div>");
+        }
+    } catch{
+        res.send("Erro Interno do servidor");
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (foundUser) {
+    
+            let submittedPass = req.body.password; 
+            let storedPass = foundUser.password; 
+    
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+            if (passwordMatch) {
+                let usrname = foundUser.username;
+                res.send(`<div align ='center'><h2>login efetuado.</h2></div><br><br><br><div align ='center'><h3>Olá ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+            } else {
+                res.send("<div align ='center'><h2>Email ou senha inválido.</h2></div><br><br><div align ='center'><a href='./login.html'>tentar novamente</a></div>");
+            }
+        }
+        else {
+            // senha falsa que gasta tempo de processamento de propósito
+            // pro usuário não saber se o erro foi no email ou senha
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+            await bcrypt.compare(req.body.password, fakePass);
+    
+            res.send("<div align ='center'><h2>Email ou senha inválido.</h2></div><br><br><div align='center'><a href='./login.html'>tentar novamente<a><div>");
+        }
+    } catch{
+        res.send("Erro Interno do servidor");
+    }
+});
+
+
+server.listen(3000, function(){
+    console.log("servidor ouvindo na porta: 3000");
+});
